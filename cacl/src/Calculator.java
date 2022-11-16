b8924884a0082a3b60c2487e9aa03274735b9411
maximos008 <113299796+maximos008@users.noreply.github.com>
2022-11-16 15:37:44 +0300

    summ and razn

--- cacl/src/Calculator.java
+++ cacl/src/Calculator.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Calculator {
     public void Calc(int a, String b, int c){
-
+        if(b == "+"){
+            System.out.println("Сумма = " + (a+c));
+        }
+        else if (b == "-") {
+            System.out.println("Разность" + (a-c));
+        }
     }
 
 }
