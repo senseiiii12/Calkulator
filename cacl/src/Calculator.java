4ddcbc496edaafe864d849edbf9d5cd64f0899d7
maximos008 <117999420+senseiiii12@users.noreply.github.com>
2022-11-16 16:03:49 +0300

    Update Calculator.java

--- cacl/src/Calculator.java
+++ cacl/src/Calculator.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Calculator {
     public void Calc(int a, String b, int c){
         if(b.equals("+")){
-            System.out.println("Сумма =" + (a+c));
+            System.out.println("Сумма = " + (a+c));
         }
         else if (b.equals("-")) {
-            System.out.println("Разность =" + (a-c));
+            System.out.println("Разность = " + (a-c));
         }
         else if (b.equals("*")){
-            System.out.println("Умножение" + (a*c));
+            System.out.println("Умножение = " + (a*c));
         }
         else if(b.equals("/")){
-            System.out.println("Деление" + (a/c));
+            System.out.println("Деление = " + (a/c));
         }
     }
 
