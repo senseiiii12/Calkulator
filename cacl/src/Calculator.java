166076939f4a87eb3e14b5e71950eb54e62828e0
Your Name <you@example.com>
2022-11-16 15:57:06 +0300

    sui

--- cacl/src/Calculator.java
+++ cacl/src/Calculator.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class Calculator {
     public void Calc(int a, String b, int c){
-        if(b == "+"){
-            System.out.println("Сумма = " + (a+c));
+        if(b.equals("+")){
+            System.out.println("Сумма =" + (a+c));
         }
-        else if (b == "-") {
-            System.out.println("Разность" + (a-c));
+        else if (b.equals("-")) {
+            System.out.println("Разность =" + (a-c));
+        }
+        else if (b.equals("*")){
+            System.out.println("Умножение =" + (a*c));
+        }
+        else if(b.equals("/")){
+            System.out.println("Деление =" + (a/c));
         }
     }
 
