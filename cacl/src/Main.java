366a7247b7165d3f681e2be6dc1f5622f8bd7344
maximos008 <117999420+senseiiii12@users.noreply.github.com>
2022-11-16 16:01:04 +0300

    asd

--- cacl/src/Main.java
+++ cacl/src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         calculator.Calc(8,"-",9);
         calculator.Calc(10,"*",2);
         calculator.Calc(8,"/",2);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
